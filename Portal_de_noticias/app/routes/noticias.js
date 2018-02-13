f3c8b6bdef8110ebf42c34c55110c8443d26545f
Gabriel-Cordeiro <gabrielsilva.ipms@gmail.com>
2018-02-13 14:05:37 -0200

    Novas views e funcionalidades do projeto

--- Portal_de_noticias/app/routes/noticias.js
+++ Portal_de_noticias/app/routes/noticias.js
@@ -1,21 +1,12 @@
-
 module.exports = function (app) {
 
-    app.get('/noticias', function (req, res) {
-        var mysql = require("mysql");
+        app.get('/noticias', function (req, res) {
 
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            //password: ''
-            database: 'portal_noticias'
+        var connection = app.config.dbConnection();
+        var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+        noticiasModel.getNoticias(function (error, result) {
+            res.render("noticias/noticias", { noticias: result });
         });
-
-        connection.query('select * from noticias', function (error, result) 
-        {
-            res.send(result);
-        });
-
-        //res.render("noticias/noticias");
     });
-}
+};
